2c536e8ce5e27bc97ed9641212107740881274ff
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-03-22 14:51:58 -0700

    This is the sprint with a stretch goal completed. Please review this one!

---
+++ contact.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Sprint Challenge - Home</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Roboto|Rubik" rel="stylesheet">
+  <link rel="stylesheet" href="css/index.css">
+
+</head>
+<body>
+<div class="container">
+        <header>
+            <img src="img/lambda-black.png" />
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="about.html">About</a>
+                <a href="#">Products</a>
+                <a href="#">Blog</a>
+                <a href="contact.html">Contact</a>
+            </nav>
+        </header>
+    <div class="heightformat">
+        <p>
+        Thank you for your message! I appreciate your feedback!
+        </p>
+    </div>
+        <footer>
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="about.html">About</a>
+                <a href="#">Products</a>
+                <a href="#">Blog</a>
+                <a href="contact.html">Contact</a>
+            </nav>
+        </footer>
+    </div><!-- container -->        
+</body>
+</html>
